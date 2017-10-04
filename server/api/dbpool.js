21edf5b2ec5de1569fa5dd9d31b76a3052741640
hates <ates.hueseyin@gmail.com>
2017-10-04 15:14:46 +0300

    product services

--- server/api/dbpool.js
+++ server/api/dbpool.js
@@ -1,4 +1,4 @@
 const mysql = require('mysql');
 const dbConfig = require('../../db-config.json');
-const connection = mysql.createPool(dbConfig);
-module.exports = connection;
+const pool = mysql.createPool(dbConfig);
+module.exports = pool;
--- server/api/productRouter.js
+++ server/api/productRouter.js
@@ -1,13 +1,43 @@
 const express = require('express');
-const db = require('./dbconnection');
+const bodyParser = require('body-parser');
+const db = require('./dbpool');
 const router = express.Router();
 
-router.get('/product/:id', function (req, res) {
-res.json({productId:req.params.id});
+const SELECT_PRODUCT = "SELECT * FROM product WHERE barcode = ?";
+const UPDATE_QUANTITY = "UPDATE product SET quantity_count = ? WHERE barcode = ?"
+
+router.use(bodyParser.json());
+
+router.get('/product/:barcode', function (req, res) {
+  db.query(SELECT_PRODUCT, [req.params.barcode], function (error, results) {
+    if (error) {
+      res.status(500).send({message: error.message});
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).send({message: 'Not found!'});
+      return;
+    }
+
+    res.json(results[0]);
+  });
 });
 
-router.post('/product/:id', function (req, res) {
-
+router.post('/product/:barcode', function (req, res) {
+  db.query(UPDATE_QUANTITY, [req.body.quantity, req.params.barcode], function (error, results) {
+    if (error) {
+      res.status(500).send({message: error.message});
+      return;
+    }
+    if (results.changedRows === 1) {
+      res.json({
+        message: 'Success'
+      });
+    } else {
+      res.status(404).send({message: 'Not found!'});
+    }
+  });
 });
 
 module.exports = router;
